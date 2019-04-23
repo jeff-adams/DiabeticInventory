5a973739207d368746a22b6e8c6f7beaa4792a75
Jeff Adams <atomicxistence@gmail.com>
2019-04-22 22:07:37 -0400

    Change database creation script for SQLite

--- sql/create-database.sql
+++ sql/create-database.sql
@@ -2,18 +2,18 @@ CREATE DATABASE EDIS
 
 CREATE TABLE Inventory
 (
-    StockNumber [NVARCHAR](32) NOT NULL PRIMARY KEY,
-    Brand [NVARCHAR](24) NOT NULL,
-    ItemType [NVARCHAR](24) NOT NULL,
+    StockNumber NUMERIC NOT NULL PRIMARY KEY,
+    Brand TEXT NOT NULL,
+    ItemType TEXT NOT NULL,
     UnitsOnHand INT NOT NULL,
-    ReorderPoint INT NOT NULL,
+    ReorderPoint INT NOT NULL
 );
 
 CREATE TABLE Prescription
 (
-    RxNumber [NVARCHAR](24) NOT NULL PRIMARY KEY,
+    RxNumber NUMERIC NOT NULL PRIMARY KEY,
     RefillsRemaining INT NOT NULL,
     LastRefillDate DATE NOT NULL,
-    StockNumber [NVARCHAR](32) NOT NULL,
+    StockNumber NUMERIC NOT NULL,
     FOREIGN KEY(StockNumber) REFERENCES Inventory(StockNumber)
 );
