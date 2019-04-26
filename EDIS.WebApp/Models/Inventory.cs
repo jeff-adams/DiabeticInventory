5fa783162ec20616496388ec2feb7dd596f56b47
Jeff Adams <atomicxistence@gmail.com>
2019-04-26 12:01:12 -0400

    Edit razor pages

--- EDIS.WebApp/Models/Inventory.cs
+++ EDIS.WebApp/Models/Inventory.cs
@@ -1,13 +1,19 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EDIS.WebApp.Models
 {
     public class Inventory
     {
+        [Display(Name = "Stock Number")]
         public string StockNumber { get; set;}
         public string Brand { get; set; }
+        [Display(Name = "Item Type")]
         public string ItemType { get; set; }
+        [Display(Name = "Units On Hand")]
         public int UnitsOnHand { get; set; }
+        [Display(Name = "Reorder Point")]
         public int ReorderPoint { get; set; }
 
         public ICollection<Prescription> Prescription { get; set; }
--- EDIS.WebApp/Models/Prescription.cs
+++ EDIS.WebApp/Models/Prescription.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EDIS.WebApp.Models
 {
     public class Prescription
     {
+        [Display(Name = "Rx Number")]
         public string RxNumber { get; set;}
+        [Display(Name = "Refills Remaining")]
         public int RefillsRemaining { get; set; }
+        [Display(Name = "Last Refill Date")]
+        [DataType(DataType.Date)]
         public DateTime LastRefillDate { get; set; }
+        [Display(Name = "Stock Number")]
         public string StockNumber { get; set; }
 
         public Inventory StockNumberNavigation { get; set; }
